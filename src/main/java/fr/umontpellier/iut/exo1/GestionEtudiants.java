719e6fd6ae9d98dcd1da2abecf94ad6e0a7f1824
Mathieu Rocci <mathieu.rocci@etu.umontpellier.fr>
2019-02-05 09:50:50 +0100

    Comparer toto et lolo 1

--- src/main/java/fr/umontpellier/iut/exo1/GestionEtudiants.java
+++ src/main/java/fr/umontpellier/iut/exo1/GestionEtudiants.java
@@ -6,8 +6,12 @@ import static java.time.Month.JANUARY;
 
 public class GestionEtudiants {
     public static void main(String[] args) {
-        Etudiant lolo=new Etudiant("Deiana","Philippe", LocalDate.of(1997, JANUARY, 31), "fifou34@caramail.com", "18 rue Morgue");
+        Etudiant lolo = new Etudiant("Deiana", "Philippe", LocalDate.of(1997, JANUARY, 31), "fifou34@caramail.com", "18 rue Morgue");
+        Etudiant toto = new Etudiant("Deiana", "Philippe", LocalDate.of(1997, JANUARY, 31), "fifou34@caramail.com", "18 rue Morgue");
+
         System.out.println(lolo.toString());
+        System.out.println(lolo==toto);
+        }
+
     }
 
-}
