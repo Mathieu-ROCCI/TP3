8289839d9ef3262b6c177cd5caceb6ffa2aa4a57
Mathieu Rocci <mathieu.rocci@etu.umontpellier.fr>
2019-02-05 09:11:34 +0100

    Création classe Etudiant + constructeur + méthode toString()

---
+++ src/main/java/fr/umontpellier/iut/exo1/Etudiant.java
@@ -0,0 +1,31 @@
+package fr.umontpellier.iut.exo1;
+
+import java.time.LocalDate;
+
+public class Etudiant {
+    private String nom;
+    private String prenom;
+    private LocalDate dateNaissance;
+    private String mail;
+    private String adressePost;
+
+    public Etudiant(String nom, String prenom, LocalDate dateNaissance, String mail, String adressePost) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.mail = mail;
+        this.adressePost = adressePost;
+    }
+
+    @Override
+    public String toString() {
+        return "Etudiant{" +
+                "nom='" + nom + '\'' +
+                ", prénom='" + prenom + '\'' +
+                ", date de naissance=" + dateNaissance +
+                ", e-mail='" + mail + '\'' +
+                ", adresse postale='" + adressePost + '\'' +
+                '}';
+    }
+}
+
